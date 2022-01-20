ea8ebb90739294c64dff42146c499f0da03be89c
ku4umid3e <kuchumid3e@gmail.com>
2022-01-20 15:09:16 +0300

    corrected the mistake

--- recogn_face.py
+++ recogn_face.py
@@ -6,8 +6,9 @@ def detect_face(img):
     image = rf.load_image_file(img)
     face_img_loc = rf.face_locations(image)
     if len(face_img_loc) == 0:
-        return "I did not find people on this photo."
         os.remove(img)
+        return "I did not find people on this photo."
+        
     if len(face_img_loc) == 1:
         return "I found 1 face on this photo."
     
